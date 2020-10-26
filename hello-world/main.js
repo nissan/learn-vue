1df40d8275a4d80103497c6517202a0627a304ea
Nissan Dookeran <Nissan.Dookeran@jjswaste.com.au>
2020-10-26 18:20:00 +1000

    add list with v-for and key binding

--- hello-world/main.js
+++ hello-world/main.js
@@ -4,6 +4,16 @@ var app = new Vue({
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         inventory: 9,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
