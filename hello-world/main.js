949a72dff4977a6833c479230d90ee50ecb0a550
Nissan Dookeran <Nissan.Dookeran@jjswaste.com.au>
2020-10-27 07:43:49 +1000

    add emit event for add-to-cart

--- hello-world/main.js
+++ hello-world/main.js
@@ -33,9 +33,6 @@ Vue.component('product', {
 
                     Add to Cart</button>
 
-                <div class="cart">
-                    <p>Cart({{cart}})</p>
-                </div>
             </div>
         </div>`,
     data() {
@@ -59,12 +56,11 @@ Vue.component('product', {
                         variantQuantity: 0
                     }
                 ],
-            cart: 0
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index) {
             this.selectedVariant = index
@@ -88,7 +84,13 @@ Vue.component('product', {
 var app = new Vue({
     el: '#app',
     data: {
-        premium:true
+        premium:true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
     }
 
 })
