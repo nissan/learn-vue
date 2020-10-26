8096c2e053ca7fbdf10088cf11d62590e46a6aad
Nissan Dookeran <Nissan.Dookeran@jjswaste.com.au>
2020-10-26 21:50:16 +1000

    use computed property that changes based on prop value

--- hello-world/main.js
+++ hello-world/main.js
@@ -15,7 +15,9 @@ Vue.component('product', {
                 <p v-show="inventory > 10"> In Stock</p>
                 <p v-show="inventory<=10 && inventory>0">Almost out of stock</p>
                 <p v-show="inventory===0">Out of stock</p>
+                <p>Shipping: {{shipping}}</p>
                 <p> User is premium: {{ premium }}</p>
+
                 <ul>
                     <li v-for="detail in details">
                         {{detail}}
@@ -77,6 +79,9 @@ Vue.component('product', {
         },
         inventory() {
             return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping(){
+            return this.premium? "Free": "2.99"
         }
     }
 })
