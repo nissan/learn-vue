e26b1de6ef9b933edca21a516595cbac2c1a5fa4
Nissan Dookeran <Nissan.Dookeran@jjswaste.com.au>
2020-10-26 18:15:04 +1000

    add list with v-for

--- hello-world/main.js
+++ hello-world/main.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        inventory: 9
+        inventory: 9,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
     }
 })
